361f2693c3385f33964b81611a57562841949aed
Mukesh Kushwaha <adhikarymaruti@gmail.com>
2024-05-28 22:16:00 +0545

    working on user post ui

--- frontend/src/components/UserHeader.jsx
+++ frontend/src/components/UserHeader.jsx
@@ -79,6 +79,27 @@ const UserHeader = () => {
           </Box>
         </Flex>
       </Flex>
+      <Flex w={"full"}>
+        <Flex
+          flex={1}
+          justifyContent={"center"}
+          borderBottom={"1.5px solid white"}
+          pb={3}
+          cursor={"pointer"}
+        >
+          <Text fontWeight={"bold"}>Threads</Text>
+        </Flex>
+        <Flex
+          flex={1}
+          justifyContent={"center"}
+          borderBottom={"1.5px solid gray"}
+          color={"gray.light"}
+          pb={3}
+          cursor={"pointer"}
+        >
+          <Text fontWeight={"bold"}>Replies</Text>
+        </Flex>
+      </Flex>
     </VStack>
   );
 };
